17a67e010f99bf05e323ec815c76837c0133cdeb
Daniel Hernández <daniel.hhuerta@gmail.com>
2025-06-16 14:46:32 +0200

    commit

---
+++ src/main/java/com/dhh/ragBasic/RagBasicApplication.java
@@ -0,0 +1,13 @@
+package com.dhh.ragBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhh/ragBasic/RagBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhh.ragBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
